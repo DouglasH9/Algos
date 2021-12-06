976b06cd354faf325c012149a5e14bbcfe34cd36
Doug Herman <douglash9882@gmail.com>
2021-12-06 17:27:08 -0600

    updated dijkstra's algorithm

--- grokkingAlgos.py
+++ grokkingAlgos.py
@@ -204,5 +204,24 @@ parents["fin"] = None
 
 processed = []
 
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
 node = find_lowest_cost_node(costs)
-while
+while node is not None:
+    cost = costs[node]
+    neighbors = d_graph[node]
+    for neighbor in neighbors.keys():
+        new_cost = cost + neighbors[neighbor]
+        if costs[neighbor] > new_cost:
+            costs[neighbor] = new_cost
+            parents[neighbor] = node
+        processed.append(node)
+        node = find_lowest_cost_node(costs)
