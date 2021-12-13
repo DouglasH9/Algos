627d6a7c7e120e388a15209815a69e8958125e7b
Doug Herman <douglash9882@gmail.com>
2021-12-13 16:00:03 -0600

    climb stairs fibonacci solution

--- JSAlgos.js
+++ JSAlgos.js
@@ -55,3 +55,29 @@ const plusOne = (digits) => {
     return digits;
     
 };
+
+// ===========================================Stair Climber===========================================
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Fibonacci solution...
+
+var climbStairs = function(n) {
+    if (n===1){
+        return n;
+    }
+    let first = 1;
+    let second = 2; 
+    for (let i = 3; i <= n; i++){
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+    return second;
+    
+};
+
+
+
+
