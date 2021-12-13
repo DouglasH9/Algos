81c37f328984ed782801822cd9cdbaf0f189ddc6
Doug Herman <douglash9882@gmail.com>
2021-12-13 14:29:37 -0600

    plus one array algo

--- JSAlgos.js
+++ JSAlgos.js
@@ -28,4 +28,30 @@ const fastMaxSubArray = (arr) => {
     return maxSub;
 }
 
-console.log(fastMaxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
+// console.log(fastMaxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
+
+// ==================================Plus one array=========================================
+// given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+// Increment the large integer by one and return the resulting array of digits.
+
+const plusOne = (digits) => {
+    if (digits.length === 1 && digits[0] === 9){
+        digits[0] = 1;
+        digits.push(0);
+        return digits;
+    }
+    for (let i = digits.length - 1; i > - 1; i--){
+        if(digits[i] === 9){
+            digits[i] = 0;
+        }
+        else {
+            digits[i] += 1
+            return digits;
+        }
+    }
+    digits[0] = 1;
+    digits.push(0);
+    return digits;
+    
+};
