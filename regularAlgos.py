01fbcddd8f3f56573187de2a401465842282c474
Doug Herman <douglash9882@gmail.com>
2021-12-16 00:41:37 -0600

    merge 2 sorted arrays

--- regularAlgos.py
+++ regularAlgos.py
@@ -47,3 +47,37 @@ def firstUniqChar(s):
         return -1
 
 # print(firstUniqChar("ddouglasherman"))
+
+# =========================================Merge 2 sorted arrays============================================
+"""
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+"""
+
+def merge(nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        # make copy of the first m elements of nums1
+        nums1_copy = nums1[:m]
+        
+        # pointers for nums1_copy and nums2
+        p1 = 0
+        p2 = 0
+        
+        # compare elements nums1_copy and nums2 and write the smallest to nums1
+        for p in range(n+m):
+            if p2 >= n or (p1 < m and nums1_copy[p1] <= nums2[p2]):
+        # make sure p1 and p2 aren't over the boundaries of their arrays
+                nums1[p] = nums1_copy[p1]
+                p1 += 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 += 1
