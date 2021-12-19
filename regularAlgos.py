b6322214865dc1cdb76633dfd8cf37846b213752
Doug Herman <douglash9882@gmail.com>
2021-12-18 19:04:34 -0600

    roman to int, and sqrt

--- regularAlgos.py
+++ regularAlgos.py
@@ -1,4 +1,6 @@
 from collections import Counter;
+from math import e, log
+# from typing import ValuesView;
 
 # Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
 
@@ -28,7 +30,7 @@ def addStrings(num1, num2):
             
         return "".join(str(x) for x in res[::-1])
 
-print(addStrings("11", "123"))
+# print(addStrings("11", "123"))
 
 # =========================================First Unique Character in a string===================================
 
@@ -101,3 +103,94 @@ def merge_two_sorted_alt(nums1, m, nums2, n):
         else:
             nums1[p] = nums2[p2]
             p2 -= 1
+
+# ================================================Finding the Sqrt of a number===============================================
+"""
+Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+"""
+
+# Pocket calculator algorithm
+
+def mySqrt1(x):
+    if x < 2:
+        return x
+    
+    left = int(e**(0.5 * log(x)))
+    right = left +1
+    return left if right * right > x else right
+
+# print(mySqrt1(4))
+
+# Binary search approach
+
+def mySqrt2(x):
+    if x < 2: 
+        return x
+    
+    left, right = 2, x//2
+
+    while left <= right:
+        pivot = left + (right - left) // 2
+        num = pivot * pivot
+        if num > x:
+            right = pivot - 1
+        elif num < x:
+            left = pivot + 1
+        else:
+            return pivot
+    return right
+
+# print(mySqrt1(102))
+
+# Newton's approach
+
+def mySqrt2(x):
+    if x < 2:
+        return x
+    x0 = x
+    x1 = (x0 + x / x0) / 2
+    while abs(x0 - x1) >= 1:
+        x0 = x1
+        x1 = (x0 + x / x0) / 2
+    return int(x1)
+
+# ====================================================Roman Numeral to Integer===================================================
+"""
+Given a roman numeral, convert it to an integer
+"""
+# starting at beginning and looping to end approach...
+
+# create map of roman numerals and their values 
+romans = {
+    "I" : 1,
+    "V" : 5,
+    "X" : 10,
+    "L" : 50,
+    "C" : 100,
+    "D" : 500,
+    "M" : 1000,
+}
+
+def romanToInt(romanNum):
+    total = 0
+    i = 0
+    while i < len(romanNum):
+        # subtraction case where a lower value appears before a higher value...
+        if i + 1 < len(romanNum) and romans[romanNum[i]] < romans[romanNum[i + 1]]:
+            total += romans[romanNum[i+1]] - romans[romanNum[i]]
+            # increase i by 2 to skip over both evaluated nums
+            i += 2
+        else:
+            total += romans[romanNum[i]]
+            i += 1
+    return total
+
+# print(romanToInt("MCDXXIII"))
+
+# back to front approach (faster and more efficient)...
+
