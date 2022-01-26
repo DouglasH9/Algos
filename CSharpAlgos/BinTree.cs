b7d767ec6aec31ccd44812cef7fa0be94ce79919
Doug Herman <douglash9882@gmail.com>
2022-01-25 20:36:55 -0600

    added comments

--- CSharpAlgos/BinTree.cs
+++ CSharpAlgos/BinTree.cs
@@ -7,9 +7,12 @@ namespace CSharpAlgos
 {
     public class Node
     {
+        // define attributes of Node
         public int val {get; set;}
         public Node left {get; set;}
         public Node right {get; set;}
+        
+        // construct Node with int val as parameter
         public Node(int val)
         {
             this.val = val;
@@ -18,7 +21,10 @@ namespace CSharpAlgos
 
     public class BinTree
     {
+        // declare valueless Node as _root
         private Node _root;
+
+        // declare BinTree object with _root Node having a value of null
         public BinTree()
         {
             _root = null;
@@ -34,30 +40,42 @@ namespace CSharpAlgos
             // if not, recur down the tree...
             InsertRecur(_root, new Node(val));
         }
+        // recursive helper function to recursively insert Nodes
         private void InsertRecur(Node root, Node newNode)
         {
+            // insert newNode as root if root is null
             if (root == null)
             {
                 root = newNode;
             }
+
+            // if newNode's value is less than the root...
             if (newNode.val < root.val)
             {
+                // insert newNode as root.left if root.left is null
                 if(root.left == null)
                 {
                     root.left = newNode;
                 }
+                // if root.left is not null recur with root.left as starting node
                 else InsertRecur(root.left, newNode);
             }
+
+            // if newNode's val is greater than root...
             else
             {
+                // insert newNode into root.right if root.right is null
                 if (root.right == null)
                     root.right = newNode;
+                
+                // otherwise, recur down with root.right and starting Node
                 else
                     InsertRecur(root.right, newNode);
             }
         }
         private void DisplayBinTreeInOrderTrav(Node root)
         {
+            // if root
             if (root == null) return;
             DisplayBinTreeInOrderTrav(root.left);
             Console.WriteLine(root.val + " ");
@@ -66,6 +84,10 @@ namespace CSharpAlgos
         public void DisplayBinTreeInOrderTrav()
         {
             DisplayBinTreeInOrderTrav(_root);
+        }
+        private void DisplayBinTreePreOrderTrav(Node root)
+        {
+
         }
     }
 
