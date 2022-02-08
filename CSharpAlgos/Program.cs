076c97c24a8d837bbfd6d8863d9872e56939a328
Doug Herman <douglash9882@gmail.com>
2022-02-07 21:48:53 -0600

    circular queue constructor and EnQueue function

--- CSharpAlgos/Program.cs
+++ CSharpAlgos/Program.cs
@@ -11,7 +11,7 @@ namespace CSharpAlgos
             int[] arr = new int[] {1,1,2,2,3,3,3,4,4,5,6,7,8,8};
             int[] arr2 = new int[] {7,1,5,3,6,4};
             // Console.WriteLine(Algos.isNumPalindrome(423));
-            string[] words = {"hello", "hell", "help", "helmet"};
+            // string[] words = {"hello", "hell", "help", "helmet"};
             // Console.WriteLine(Algos.LongestCommonPrefix(words));
             // Console.WriteLine(Algos.ValidParens("()"));
             // Array.ForEach(Algos.FrontLoadDuplicates(arr), Console.WriteLine);
@@ -21,20 +21,20 @@ namespace CSharpAlgos
             // List<string> depths = System.IO.File.ReadLines("adventDayOne.txt").ToList();
             // Console.WriteLine(Algos.AdventDepths(depths));
             // Console.WriteLine(Algos.LengthOfLastWord("a "));
-            BinTree newTree = new BinTree();
-            Node root = new Node(8);
-            newTree.InsertToBinTree(root.val);
-            newTree.InsertToBinTree(7);
-            newTree.InsertToBinTree(4);
-            newTree.InsertToBinTree(5);
-            newTree.InsertToBinTree(1);
-            newTree.InsertToBinTree(21);
-            newTree.InsertToBinTree(8);
-            newTree.InsertToBinTree(14);
-            newTree.InsertToBinTree(11);
-            newTree.InsertToBinTree(10);
-            BinTree symTree = new BinTree();
-            symTree.InsertToBinTree(2);
+            // BinTree newTree = new BinTree();
+            // Node root = new Node(8);
+            // newTree.InsertToBinTree(root.val);
+            // newTree.InsertToBinTree(7);
+            // newTree.InsertToBinTree(4);
+            // newTree.InsertToBinTree(5);
+            // newTree.InsertToBinTree(1);
+            // newTree.InsertToBinTree(21);
+            // newTree.InsertToBinTree(8);
+            // newTree.InsertToBinTree(14);
+            // newTree.InsertToBinTree(11);
+            // newTree.InsertToBinTree(10);
+            // BinTree symTree = new BinTree();
+            // symTree.InsertToBinTree(2);
             // symTree.InsertToBinTree(1);
             // symTree.InsertToBinTree(1);
             // newTree.DisplayBinTreeInOrderTrav();
@@ -44,13 +44,13 @@ namespace CSharpAlgos
             // newTree.PrintTree(newTree._root, BinTree.BinTreeTraversal.PreOrder);
             // Console.WriteLine("__________________________");
             // newTree.PrintTree(newTree._root, BinTree.BinTreeTraversal.PostOrder);
-            Console.WriteLine("__________________________");
+            // Console.WriteLine("__________________________");
             // newTree.LevelOrderTraversal(newTree._root);
-            Console.WriteLine("__________________________");
+            // Console.WriteLine("__________________________");
             // Console.WriteLine(newTree.MaximumDepth(newTree._root));
             // symTree.LevelOrderTraversal(symTree._root);
             // Console.WriteLine(symTree.IsSymmetrical(symTree._root));
-            Console.WriteLine(newTree.HasPathSum(newTree._root, 24));
+            // Console.WriteLine(newTree.HasPathSum(newTree._root, 24));
         }
     }
 }
--- CSharpAlgos/Stack.cs
+++ CSharpAlgos/Stack.cs
@@ -4,8 +4,33 @@ using System.Collections.Generic;
 namespace CSharpAlgos
 {
     // module that will utilize the STACK data structure for various algos
-    public class CirularQueue
+    public class CircularQueue
     {
-        
+        private int[] queue;
+        private int headIndex;
+        private int count;
+        private int capacity;
+
+        public CircularQueue(int n)
+        {
+            this.capacity = n;
+            this.queue = new int[n];
+            this.headIndex = 0;
+            this.count = 0;
+        }
+
+        public bool EnQueue(int val)
+        {
+            // if queue is full, return false, since there's no more room
+            if (this.count == this.capacity)
+            {return false;}
+
+            // place new value in the space that is the remainder of the capacity divided by the headIndex + the count
+            this.queue[(this.headIndex + this.count) % this.capacity] = val;
+            // increase the count by one
+            this.count += 1;
+            // return true to show that val was inserted
+            return true;
+        }
     }
 }
